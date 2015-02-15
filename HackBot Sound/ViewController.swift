d310a4943277d97b0d40d32f71631cef1ae22509
Jack Cook <jack@jackcook.nyc>
2015-02-15 10:44:48 -0500

    High five! Also log label

--- HackBot Sound/ViewController.swift
+++ HackBot Sound/ViewController.swift
@@ -16,11 +16,14 @@ class ViewController: UIViewController, GCDAsyncSocketDelegate, AVSpeechSynthesi
     var socket: GCDAsyncSocket!
     var tag = 0
     
+    @IBOutlet var logLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if SSKeychain.passwordForService("hackbot", account: "spotify") != nil {
             authenticateSpotify()
+            log("Authenticated Spotify")
         } else {
             let spotifyAuth = SPTAuth.defaultInstance()
             let spotifyLoginURL = spotifyAuth.loginURLForClientId(spotifyClientID, declaredRedirectURL: NSURL(string: spotifyCallbackURL), scopes: [SPTAuthStreamingScope, SPTAuthUserLibraryReadScope])
@@ -32,28 +35,32 @@ class ViewController: UIViewController, GCDAsyncSocketDelegate, AVSpeechSynthesi
         socket.connectToHost("irc.esper.net", onPort: 6667, error: nil)
         sendData("NICK hackbotsound\r\n")
         sendData("USER hackbot irc.esper.net bla :HackBot\r\n")
+        log("Signing into IRC")
     }
     
     func socket(sock: GCDAsyncSocket!, didReadData data: NSData!, withTag tag: Int) {
         var message = NSString(data: data, encoding: NSUTF8StringEncoding)!
         message = message.stringByReplacingOccurrencesOfString("\n", withString: "")
         print(message)
+//        log(message)
         
         var args = message.componentsSeparatedByString(" ") as [String]
         
         if args[0] as NSString == "PING" {
             sendData("PONG \(args[1])")
+            log("Pong")
         }
         
         if message.rangeOfString(":End of /MOTD command").location != NSNotFound {
             sendData("JOIN #hackcooper")
+            log("Authenticated with IRC")
         }
         
         if args.count >= 4 {
             args.removeAtIndex(0)
             args.removeAtIndex(0)
             args.removeAtIndex(0)
-            args[0] = args[0].stringByReplacingOccurrencesOfString(":", withString: "")
+            args[0] = args[0].stringByReplacingOccurrencesOfString(":", withString: "").stringByReplacingOccurrencesOfString("\r", withString: "")
             
             if args[0] == "!play" {
                 args.removeAtIndex(0)
@@ -71,17 +78,27 @@ class ViewController: UIViewController, GCDAsyncSocketDelegate, AVSpeechSynthesi
                     
                     if let ts = lp.items {
                         self.trackToPlay = ts[0] as SPTPartialTrack
+                        self.log("Playing \(self.trackToPlay.name)")
                     }
                 })
             } else if args[0] == "!say" {
                 args.removeAtIndex(0)
                 let speechText = " ".join(args)
+                log("Saying \(speechText)")
                 
                 let synthesizer = AVSpeechSynthesizer()
                 synthesizer.delegate = nil
                 let utterance = AVSpeechUtterance(string: speechText)
                 utterance.rate = AVSpeechUtteranceMinimumSpeechRate
                 synthesizer.speakUtterance(utterance)
+            } else if args[0] == "!hi5" {
+                let synthesizer = AVSpeechSynthesizer()
+                synthesizer.delegate = nil
+                let utterance = AVSpeechUtterance(string: "High five.")
+                utterance.rate = 0.15
+                synthesizer.speakUtterance(utterance)
+            } else {
+                println(args[0])
             }
         }
     
@@ -90,6 +107,10 @@ class ViewController: UIViewController, GCDAsyncSocketDelegate, AVSpeechSynthesi
     
     func socket(sock: GCDAsyncSocket!, didConnectToHost host: String!, port: UInt16) {
         sock.readDataWithTimeout(-1, tag: 0)
+    }
+    
+    func socketDidDisconnect(sock: GCDAsyncSocket!, withError err: NSError!) {
+        log("Lost connection to IRC")
     }
     
     func speechSynthesizer(synthesizer: AVSpeechSynthesizer!, didFinishSpeechUtterance utterance: AVSpeechUtterance!) {
@@ -103,5 +124,9 @@ class ViewController: UIViewController, GCDAsyncSocketDelegate, AVSpeechSynthesi
     func sendData(string: String) {
         socket.writeData("\(string)\r\n".dataUsingEncoding(NSUTF8StringEncoding), withTimeout: -1, tag: tag)
         tag += 1
+    }
+    
+    func log(string: String) {
+        logLabel.text = "\(logLabel.text!)\n\(string)"
     }
 }
